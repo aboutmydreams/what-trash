8593f2ea751785ee082392b6c78b4a11f2011857
邓文浩 <aboutmydreams@163.com>
2019-07-02 23:24:06 +0800

    write readme

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # what-trash
-垃圾分类 api 
+
+为了更好的垃圾分类
+
+## 后端运行
+
+默认 1028 端口，可在 run.py 中修改
+
+```bash
+git clone https://github.com/aboutmydreams/what-trash.git
+cd what-trash
+python run.py
+```
+
+### url: /trash
+
+#### method: GET
+
+#### param
+
+```json
+{
+  "name": "陶瓷"
+}
+```
+
+### response:
+
+```json
+{
+  "status": 1,
+  "content": "陶瓷属于干垃圾哦~"
+}
+```
+
+#### api 说明
+
+status = 1, 返回已知解答
+status = 0, 返回猜测解答
--- run.py
+++ run.py
@@ -10,10 +10,10 @@ false = null = true = ""
 app = Flask(__name__)
 
 
-@app.route('/trash', methods=['POST'])
+@app.route('/trash', methods=['GET'])
 def what_trash():
-    trash_name = request.form['trash']
-    res = trash.trash(trash_name)
+    trashname = request.args.get('name')
+    res = trash.trash(trashname)
     status = 0
     if '属于' in res:
         status = 1
@@ -21,8 +21,11 @@ def what_trash():
         'status': status,
         'content': res
     }
-    return json.dumps(ans)
+    ans = make_response(json.dumps(ans))
+    ans.headers['Access-Control-Allow-Origin'] = '*'
+    # ans.headers['Content-Type'] = 'application/json'
+    return ans
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=1026)  # host='0.0.0.0', port=80,
+    app.run(debug=True, host='0.0.0.0', port=1028)  # host='0.0.0.0', port=80,
